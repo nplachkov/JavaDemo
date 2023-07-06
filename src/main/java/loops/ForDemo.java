a8fdcadfa0905b2b17fb5681737504279ed3578a
nplachkov <nikito12386@gmail.com>
2023-07-06 17:49:15 +0300

    For Loops

---
+++ src/main/java/loops/ForDemo.java
@@ -0,0 +1,21 @@
+package loops;
+
+import java.util.Arrays;
+
+public class ForDemo {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++){
+            System.out.printf("Current value of i (index) is: %d\n", i);
+        }
+        System.out.println("------------------------------------");
+
+        for (int i = 1; i <= 20; i *= 2) {
+            System.out.printf("Current value of i (index) is: %d\n", i);
+
+        }
+        System.out.println("------------------------------------");
+
+
+    }
+}
