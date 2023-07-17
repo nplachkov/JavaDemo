9ca48ccc7892f2f4c76525c0f26f00dee7a8ba86
nplachkov <nikito12386@gmail.com>
2023-07-17 17:02:33 +0300

    OOP

--- src/main/java/oop/Dog.java
+++ src/main/java/oop/Dog.java
@@ -2,11 +2,59 @@ package oop;
 
 public class Dog {
     //characteristics
-    public String breed;
-    public float weight;
-    public String sex;
-    public byte age;
-    public String name;
+    private String breed;
+    private float weight;
+    private String sex;
+    private byte age;
+    private String name;
+
+    //Getter & Setter
+
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        if (age >= 0) {
+            this.age = age;
+        }else {
+            System.out.println("error");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     //actions
     public void bark(){
--- src/main/java/oop/Test.java
+++ src/main/java/oop/Test.java
@@ -3,24 +3,22 @@ package oop;
 public class Test {
     public static void main(String[] args) {
         Dog Hera = new Dog();
-        Hera.name = "Hera";
-        Hera.age = 12;
-        Hera.sex = "Female";
-        Hera.breed = "Boxer";
-        Hera.weight = 28.5f;
+        Hera.setName("Hera");
+        Hera.setAge((byte) 12);
+        Hera.setSex("Female");
+        Hera.setBreed("Boxer");
+        Hera.setWeight(28.5f);
 
         Dog Leo = new Dog();
-        Leo.name = "Leo";
-        Leo.age = 12;
-        Leo.sex = "Male";
-        Leo.breed = "Pitbull";
-        Leo.weight = 25.0f;
+        Leo.setName("Leo");
+        Leo.setAge((byte) 12);
+        Leo.setSex("Male");
+        Leo.setBreed("Pitbull");
+        Leo.setWeight(25.0f);
 
         Hera.bark();
-        Hera.eat();
-        Hera.sleeps();
-
         Leo.bark();
 
+        System.out.println(Hera.getAge());
     }
 }
